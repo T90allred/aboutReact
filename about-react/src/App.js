8224d5f2cfcb26ae4fe58ddc1d895430578fd0c4
Derek Allred <derekmallred@gmail.com>
2017-11-04 09:23:54 -0600

    initail setup of react app

---
+++ about-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is an app all about React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
