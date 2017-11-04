6ca89565712f41f96346d221ec549efe227673cc
Derek Allred <derekmallred@gmail.com>
2017-11-04 09:46:47 -0600

    edit of App component

--- about-react/src/App.js
+++ about-react/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +8,8 @@ class App extends Component {
         <h1>This is an app all about React</h1>
       </div>
     );
+    // manually done with React.createElement... the above jsx gets run just like the next line
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Manually done with react methods'));
   }
 }
 
